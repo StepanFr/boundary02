a4cd502ccf8b5798de37c89055a4ff8c3d9066f1
StepanFr <stepanfrwind@yandex.ru>
2024-06-05 00:10:16 +0300

    edit CMakeLists.txt

---
+++ _build/CMakeFiles/Cpp-Software-Design.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cpp-Software-Design.dir/Main.cpp.o"
+  "CMakeFiles/Cpp-Software-Design.dir/Main.cpp.o.d"
+  "Cpp-Software-Design"
+  "Cpp-Software-Design.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cpp-Software-Design.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _build/CMakeFiles/Cpp-Software-Design.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Cpp-Software-Design.
+# This may be replaced when dependencies are built.
